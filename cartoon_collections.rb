2deaf84618f936bda296377e04d1b47f5d7da966
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-02 17:08:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
     valid_calls.each do |int_ele|
-   element = valid_calls[int_ele]
+   element = valid_calls
  end 
   end 
 end
