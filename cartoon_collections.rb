8985dc9c8b89bc05ddccf2221aeff495bc2d15cf
Jonathan Brinson <jonathanbrinsontenor@gmail.com>
2020-12-02 17:09:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,8 +29,7 @@ def find_valid_calls(planeteer_calls)
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
-    valid_calls.each do |int_ele|
-   element = valid_calls
+   element = valid_calls.find 
  end 
   end 
 end
